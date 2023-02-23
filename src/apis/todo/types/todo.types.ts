8ea23e28c30e8ca95699fd257d236ca6fdb2491c
jiyeon <a1919361@gmail.com>
2023-02-24 00:04:24 +0900

    feat : todo 타입 작성

---
+++ src/apis/todo/types/todo.types.ts
@@ -0,0 +1,9 @@
+// auth의 경우 서버에서 받는 데이터, 타입이 토큰뿐이어서 대부분 components/auth/types에 들ㅇ있다
+// 투두의 경우 서버에서 받는 타입을 기준으로 컴포넌트에서 가져다 써도 될거같다
+
+export type TodoData = {
+  id: number;
+  todo: string;
+  isComplete: boolean;
+  userId: number;
+};
