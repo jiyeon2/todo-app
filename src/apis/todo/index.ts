e47f7ab101baf832cf59ac9d9610f333fb4dfa76
jiyeon <a1919361@gmail.com>
2023-02-23 23:31:30 +0900

    feat : todoClient 생성, interceptor로 토큰 저장

--- src/apis/todo/index.ts
+++ src/apis/todo/index.ts
@@ -1,1 +1,17 @@
-export {};
+import axios from 'axios';
+import { getToken } from 'utils/localStorage';
+
+const baseURL = 'https://pre-onboarding-selection-task.shop';
+
+export const todoClient = axios.create({
+  baseURL: `${baseURL}/todo/`,
+});
+
+todoClient.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
