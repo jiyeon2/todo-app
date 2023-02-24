dc28a0e94ea045408f235fbaa490f1dfe6449eb6
jiyeon <a1919361@gmail.com>
2023-02-24 09:29:57 +0900

    refactor : 투두 프로바이더에서 todos가져오도록 변경

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,8 +1,9 @@
-import { TodoData } from 'apis/todo/types/todo.types';
-import React from 'react';
+import { TodoContext } from 'context/todoContext';
+import React, { useContext } from 'react';
 import { TodoListItem } from './TodoListItem';
 
-const TodoList = ({ todos }: { todos: TodoData[] }) => {
+const TodoList = () => {
+  const { todos } = useContext(TodoContext);
   if (!todos.length) {
     return <p>작성된 할 일이 없어요. 새로운 할 일을 작성해보세요</p>;
   }
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,25 +1,13 @@
-import { TodoData } from 'apis/todo/types/todo.types';
 import TodoAddSection from 'components/todo/TodoAddSection';
 import TodoList from 'components/todo/TodoList';
-import React from 'react';
-
-const todos: TodoData[] = Array(2)
-  .fill(0)
-  .map((_, index) => ({
-    id: index + 1,
-    todo: '청소',
-    isComplete: false,
-    userId: 1,
-  }));
+import { TodoProvider } from 'context/todoContext';
 
 const Todo = () => {
-  // 투두 프로바이더로 감싸기
-
   return (
-    <div>
+    <TodoProvider>
       <TodoAddSection />
-      <TodoList todos={todos} />
-    </div>
+      <TodoList />
+    </TodoProvider>
   );
 };
 
