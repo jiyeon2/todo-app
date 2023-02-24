b690e03448a05dd62f988b49a61b5cc1e7109759
jiyeon <a1919361@gmail.com>
2023-02-24 10:40:45 +0900

    refactor : 투두 수정 컴포넌트 분리

---
+++ src/components/todo/TodoItemEditSection.tsx
@@ -0,0 +1,47 @@
+import { TodoData } from 'apis/todo/types/todo.types';
+import React, { useContext } from 'react';
+import { ArrowUturnLeftIcon, ArchiveBoxArrowDownIcon } from '@heroicons/react/24/solid';
+import IconButton from 'components/common/IconButton';
+import { TodoListItemCheckbox } from './TodoListItemCheckbox';
+import useInput from 'hooks/useInput';
+import { TodoContext } from 'context/todoContext';
+
+export function TodoItemEditSection({
+  todo,
+  finishEditMode,
+}: {
+  todo: TodoData;
+  finishEditMode: () => void;
+}) {
+  const { value, handleInput } = useInput(todo.todo);
+  const { editTodo } = useContext(TodoContext);
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const editedContent = value.trim();
+    if (!editedContent) return;
+    editTodo({ id: todo.id, isCompleted: todo.isCompleted, todo: editedContent }).then(() => {
+      finishEditMode();
+    });
+  };
+  return (
+    <article className="flex items-center gap-2 text-lg">
+      <TodoListItemCheckbox todo={todo} />
+      <form onSubmit={handleSubmit} className="flex flex-1 gap-2 ">
+        <input
+          className="flex-1 rounded-md ring-2 ring-black"
+          autoFocus
+          value={value}
+          onChange={handleInput}
+        />
+        <IconButton size="sm" type="submit">
+          <ArchiveBoxArrowDownIcon className="text-white" />
+        </IconButton>
+      </form>
+
+      <IconButton size="sm" variant="warning" onClick={finishEditMode}>
+        <ArrowUturnLeftIcon className="text-white" />
+      </IconButton>
+    </article>
+  );
+}
--- src/components/todo/TodoListItem.tsx
+++ src/components/todo/TodoListItem.tsx
@@ -1,13 +1,9 @@
 import { TodoData } from 'apis/todo/types/todo.types';
 import React, { useState } from 'react';
-import {
-  PencilIcon,
-  TrashIcon,
-  ArrowUturnLeftIcon,
-  ArchiveBoxArrowDownIcon,
-} from '@heroicons/react/24/solid';
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid';
 import IconButton from 'components/common/IconButton';
 import { TodoListItemCheckbox } from './TodoListItemCheckbox';
+import { TodoItemEditSection } from './TodoItemEditSection';
 
 export function TodoListItem({ todo }: { todo: TodoData }) {
   const [editMode, setEditMode] = useState(false);
@@ -38,28 +34,6 @@ export function TodoListItem({ todo }: { todo: TodoData }) {
 
       <IconButton size="sm" variant="warning">
         <TrashIcon className="text-white" />
-      </IconButton>
-    </article>
-  );
-}
-
-function TodoItemEditSection({
-  todo,
-  finishEditMode,
-}: {
-  todo: TodoData;
-  finishEditMode: () => void;
-}) {
-  return (
-    <article className="flex items-center gap-2 text-lg">
-      <TodoListItemCheckbox todo={todo} />
-      <input className="flex-1 rounded-md ring-2 ring-black" defaultValue={todo.todo} autoFocus />
-      <IconButton size="sm">
-        <ArchiveBoxArrowDownIcon className="text-white" />
-      </IconButton>
-
-      <IconButton size="sm" variant="warning" onClick={finishEditMode}>
-        <ArrowUturnLeftIcon className="text-white" />
       </IconButton>
     </article>
   );
